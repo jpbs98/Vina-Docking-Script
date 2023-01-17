ac4d7c4cc6335a31f65607926cd8f69ee5b3655e
jpbs98 <114289362+jpbs98@users.noreply.github.com>
2023-01-17 16:53:07 +0000

    Add files via upload

---
+++ merge_run_analyze.py
@@ -0,0 +1,42 @@
+"""Vina Virtual Screening --> Run & Analyze"""
+import os
+
+# get path
+path = os.getcwd()
+
+# run iterative vina command on all ligands --> change receptor name
+for file in os.listdir(path):
+    if file == "STEAP1Hem.pdbqt":
+        continue
+    elif file.endswith(".pdbqt"):
+        os.system(
+            f"vina --config conf.txt --ligand {file} --log {file}_log.log")
+
+os.system("tail -n11 *.log > results.txt")
+
+print("\n")
+print("Starting analysis...")
+dct = {}
+
+# iterate over log files and output results in dict form
+for file in os.listdir(path):
+    if file.endswith(".pdbqt_log.log"):
+        with open(file, "r") as f:
+            for i, line in enumerate(f):
+                if i == 27:
+                    dct[file] = float(line.split()[1])
+                elif i > 27:
+                    break
+
+# Order the dictionary to output top hits first
+ordered_dict = {k: v for k, v in sorted(
+    dct.items(), key=lambda item: item[1])}
+
+# Output to .txt file
+with open("results_sorted.txt", "w") as o:
+    o.write("Sorted Docking Results\n\n")
+    for k, v in ordered_dict.items():
+        o.write(f"{k}: {v}\n")
+
+print("\n")
+print("Analysis complete. See your results in the results_sorted.txt file.")
